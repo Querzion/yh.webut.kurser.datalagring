b9d5ed6647afeeffd716401bf2e3ed9fed782c5c
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-29 08:00:42 +0100

    initial commit

--- week_03/learn_012_eagerloading_efc/Data/Entities/OrdersEntity.cs
+++ week_03/learn_012_eagerloading_efc/Data/Entities/OrdersEntity.cs
@@ -1,6 +1,6 @@
 namespace Data.Entities;
 
-public class OrderEntity
+public class OrdersEntity
 {
     
 }
---
+++ week_03/learn_012_eagerloading_efc/Data/Scripts/SQLQuery.sql
@@ -0,0 +1,5 @@
+-- SQL Script
+-- Created on 29/01/2025 by querzion
+
+-- Write your SQL queries below:
+SELECT * FROM TableName;
