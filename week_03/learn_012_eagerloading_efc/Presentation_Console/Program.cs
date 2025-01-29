89e92103ff8a6a872da7a5438c446edb5813d216
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-29 16:47:17 +0100

    Presentation Created with some kind of json file creation and readout directly to console.

---
+++ week_03/learn_012_eagerloading_efc/Presentation_Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using Business.Interfaces;
+using Business.Services;
+using Data.Contexts;
+using Data.Helpers;
+using Data.Interfaces;
+using Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var connectionString = DatabaseHelper.GetDatabaseConnectionString();
+
+var services = new ServiceCollection()
+    .AddDbContext<DataContext>(x => x.UseSqlite(connectionString))
+    .AddScoped<IOrderRepository, OrderRepository>()
+    .AddScoped<IOrderService, OrderService>();
+
+var serviceProvider = services.BuildServiceProvider();
+var orderService = serviceProvider.GetRequiredService<IOrderService>();
+
+var orders = await orderService.GetAllOrdersAsync();
+
+JsonSerializerOptions options = new()
+{
+    WriteIndented = true,
+    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles,
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+};
+
+foreach (var order in orders)
+{
+    var json = JsonSerializer.Serialize(order, options);
+    Console.WriteLine(json);
+}
+
+Console.ReadKey();
